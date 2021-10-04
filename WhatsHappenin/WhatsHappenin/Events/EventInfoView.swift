04d73768bce4be62cb94841c222c2b5dcd434cbf
Prithik Karthikeyan <prithik2000@gmail.com>
2021-10-04 12:49:24 -0400

    EventDetails

---
+++ WhatsHappenin/WhatsHappenin/Events/EventInfoView.swift
@@ -0,0 +1,49 @@
+//
+//  createEvent.swift
+//  WhatsHappenin
+//
+//  Created by Prithik Karthikeyan on 9/21/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct EventInfoView : View {
+            
+    var body: some View {
+        Form{
+            Section{
+            Text("Event Name:")
+            Text("Event Description:")
+                .lineLimit(4)
+                .multilineTextAlignment(.leading)
+                .frame(minWidth: 100, maxWidth: 200, minHeight: 100, maxHeight: .infinity, alignment: .topLeading)
+                
+            }
+            Section{
+            Text("Starts:")
+            Text("Ends:")
+            }
+            Text("Address:")
+                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+            Text("Phone Number:")
+            Text("Email:")
+            Section{
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("RSVP")
+            })
+                
+            }
+            }
+            .navigationBarTitle(Text("Event Details"))
+        }
+    
+}
+
+
+
+struct EventInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventInfoView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
