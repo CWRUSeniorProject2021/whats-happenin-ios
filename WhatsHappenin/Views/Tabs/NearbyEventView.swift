0c82fe39b72c0f27e2da125ede240c410a3e8e84
Eddie Xu <erx@case.edu>
2021-11-14 20:41:14 -0500

    Added searchable, need to make custom search?

--- WhatsHappenin/Views/Tabs/NearbyEventView.swift
+++ WhatsHappenin/Views/Tabs/NearbyEventView.swift
@@ -11,30 +11,34 @@ import SwiftUIRefresh
 struct NearbyEventView: View {
    
     @ObservedObject var controller = EventsListController.sharedInstance
-    @State private var searchText: String = ""
+    @State private var searchText = ""
     @State var isRefreshing: Bool = false
     
     var body: some View {
-        List($controller.nearbyEvents) { $event in
-            EventRow(event: $event, controller: controller)
-        }
-        .padding(EdgeInsets(top: 44, leading: 0, bottom: 24, trailing: 0))
-        .edgesIgnoringSafeArea(.all)
-        .listStyle(PlainListStyle())
-        .pullToRefresh(isShowing: $isRefreshing) {
-            controller.loadNearbyEvents()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                self.isRefreshing = false
-                            }
-        }
-        .onChange(of: self.isRefreshing) { value in
-        }
-        .toolbar {
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                NavigationLink(destination: CreateEvent()) {
-                    Image(systemName: "plus")
-                }
-              }
+        NavigationView{
+//            SearchBar(text1: $searchText)
+            List($controller.nearbyEvents) { $event in
+                EventRow(event: $event, controller: controller)
+            }
+            .searchable(text: $searchText)
+            .padding(EdgeInsets(top: 44, leading: 0, bottom: 24, trailing: 0))
+            .edgesIgnoringSafeArea(.all)
+            .listStyle(PlainListStyle())
+            .pullToRefresh(isShowing: $isRefreshing) {
+                controller.loadNearbyEvents()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                    self.isRefreshing = false
+                                }
+            }
+            .onChange(of: self.isRefreshing) { value in
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: CreateEvent()) {
+                        Image(systemName: "plus")
+                    }
+                  }
+            }
         }
     }
 }
