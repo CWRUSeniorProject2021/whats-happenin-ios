d4c2a18f1aafb622b175e302311ab9efbdf84d1a
Eddie Xu <erx@case.edu>
2021-11-28 21:41:49 -0500

    Finished editing profile

--- WhatsHappenin/Views/Auth/EditProfileInfo.swift
+++ WhatsHappenin/Views/Auth/EditProfileInfo.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct EditProfileInfo: View {
+    @SwiftUI.Environment(\.presentationMode) var presentationMode
+    @ObservedObject var controller = ProfilePageController.sharedInstance
+
+    @State var showAlert = false
+    @State var showChanged = false
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var userName = ""
+    @State private var email = ""
+    @State private var schoolName = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            Section(header: Text("Profile Information")){
+                TextField("First Name", text: $firstName)
+                TextField("Last Name", text: $lastName)
+                TextField("Username", text: $userName)
+                TextField("Email Address", text: $email)
+            }
+        }
+        Button {
+            print("Save Changes Tapped")
+            // If saved changes successful, do the presentationMode.wrappedValue.dismiss(), else do a popup error
+            let requestData: [String: Any] = [
+                "first_name": firstName,
+                "last_name": lastName,
+                "username": userName,
+                "email": email,
+            ] as [String: Any]
+            WHAPI.sharedInstance.user.child("\(controller.myProfile?.id ?? 1)").request(.patch, json: requestData)
+                .onSuccess { response in
+                    GlobalKeychain.set(email, forKey: Keys.Auth.UID)
+                    GlobalKeychain.set(email, forKey: "email")
+                    self.presentationMode.wrappedValue.dismiss()
+                    self.showAlert = true
+                    controller.loadMyProfile()
+                }
+                .onFailure { error in
+                    self.showChanged = true
+                    let errorMessage: GenericResponse<Empty>? = WHAPI.sharedInstance.parseErrors(error)
+                    print(errorMessage)
+                        
+                    // HANDLE FAILURE HERE
+                }
+
+            
+        }
+        label : {
+            Text("Save Changes")
+                .bold()
+                .frame(width: 260, height: 50)
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Saved Changes"),
+                message: Text("Your profile information has been updated"),
+                dismissButton: .default(Text("Close"))
+            )
+        }
+        .alert(isPresented: $showChanged) {
+            Alert(
+                title: Text("Invalid Input"),
+                message: Text("Please Enter Information For All Fields"),
+                dismissButton: .default(Text("Close"))
+            )
+        }
+        .padding(.bottom, 10)
+        .navigationBarTitle("Edit Profile", displayMode: .inline)
     }
 }
 
